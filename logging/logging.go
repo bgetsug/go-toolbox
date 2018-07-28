18aaf24f2c30ebe3bb9cb7bb901fe07115600163
Ben Getsug <ben@s2nventures.com>
2018-07-28 02:09:29 -0500

    logging

--- logging/logging.go
+++ logging/logging.go
@@ -8,14 +8,6 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	// Logger is a global instance of a zap Logger
-	Logger *zap.Logger
-
-	// Log is a global instance of a zap SugaredLogger
-	Log *zap.SugaredLogger
-)
-
 func Init(env config.Environment) {
 	logger, err := zap.NewProduction()
 
@@ -29,16 +21,13 @@ func Init(env config.Environment) {
 		if err != nil {
 			log.Fatal(err)
 		}
-
 	}
 
-	Logger = logger
-	Log = Logger.Sugar()
-	zap.ReplaceGlobals(Logger)
+	zap.ReplaceGlobals(logger)
 }
 
 // NewModuleLog return a zap SugaredLogger configured to log the
 // specified name as the "module" field in the logging context
 func NewModuleLog(name ...string) *zap.SugaredLogger {
-	return Log.Named(strings.Join(name, "."))
+	return zap.S().Named(strings.Join(name, "."))
 }
